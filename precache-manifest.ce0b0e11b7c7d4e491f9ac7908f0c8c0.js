a1198b0e03aa95f733cb2396522d71cc2121768a
dmitryweiner <dmitry.weiner@gmail.com>
2020-05-28 10:24:44 +0700

    Added code

---
+++ precache-manifest.ce0b0e11b7c7d4e491f9ac7908f0c8c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344f97da9d46da847e1d8b1c5f4fa1b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf0112be9504c2cab5f5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "40327c8779c13a0cf881",
+    "url": "/static/js/2.c510391d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c510391d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0112be9504c2cab5f5",
+    "url": "/static/js/main.b95227c3.chunk.js"
+  },
+  {
+    "revision": "7cd5f80cf8615068f826",
+    "url": "/static/js/runtime-main.e5e96c8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
